44c2e28099750c35b658d05b00a725e8a975bb15
Julia Guenther <juliag1@alumni.stanford.edu>
2017-01-23 21:18:55 -0800

    Adding magnitude.py

---
+++ magnitude.py
@@ -0,0 +1,17 @@
+import math
+
+def caluculate_magnitude(vect):
+    """
+    This function calulates the magnitude of a vector of n coordinates
+
+    >>> caluculate_magnitude([-0.221,7.437])
+    7.440282924728065
+
+    >>> caluculate_magnitude([8.813,-1.331,-6.247])
+    10.884187567292289
+
+    """
+    total = 0
+    for i in xrange(len(vect)):
+        total += (vect[i] * vect[i])
+    return math.sqrt(total)
