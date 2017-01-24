f793f636255ff3452d48af7f01896e1996f91414
Julia Guenther <juliag1@alumni.stanford.edu>
2017-01-23 21:37:58 -0800

    Added try/except for zero vector in normalize.py

--- normalize.py
+++ normalize.py
@@ -10,11 +10,15 @@ def normalize(vect):
     >>> normalize([1.996,3.108,-4.554])
     [0.3404012959433014, 0.5300437012984873, -0.7766470449528029]
     """
-    vect_magnitude = caluculate_magnitude(vect)
+    try: 
+        vect_magnitude = caluculate_magnitude(vect)
 
-    factor = 1/vect_magnitude
+        factor = 1/vect_magnitude
 
-    for i in xrange(len(vect)):
-        vect[i] = vect[i] * factor
-    return vect
+        for i in xrange(len(vect)):
+            vect[i] = vect[i] * factor
+        return vect
+    except ZeroDivisionError:
+        raise Exception('Cannot normalize the zero vector')
+        
    
